70006a742fb02aa58e7c3a4202ca4eded89b4b33
Erik Olsen <erik.olsen@milespartnership.com>
2021-07-24 03:07:50 -0400

    add axios and json server to part 2 phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,14 +1,10 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Filter from "./Filter";
 import PersonForm from "./PersonForm";
 import Persons from "./Persons";
+import axios from "axios";
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [results, setResults] = useState(persons);
   const [newStr, setNewStr] = useState("");
 
@@ -34,6 +30,14 @@ const App = () => {
     setResults([...arr]);
     setNewStr(str);
   }, []);
+  const axiosHook = () => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  };
+  useEffect(axiosHook, []);
   return (
     <div>
       <h2>Phonebook</h2>
