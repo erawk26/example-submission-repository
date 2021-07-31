75fb7728fc5440c16720f477315e87204aee7eee
Erik Olsen <erik.olsen@milespartnership.com>
2021-07-31 16:38:52 -0400

    Exercise 3.13 - 3.18 - missed files

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -13,95 +13,94 @@ const App = () => {
   const [newStr, setNewStr] = useState("");
   const [errorMsg, setErrorMsg] = useState(null);
   const [successMsg, setSuccessMsg] = useState(null);
-  const [telephoneInputVal, setTelephoneInputVal] = useState('');
+  const [telephoneInputVal, setTelephoneInputVal] = useState("");
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
     const evtName = evt.target.elements.name.value;
     if (evtName) {
-      //   const person = persons.find(
-      //     (p) => p.name.toLowerCase() === evtName.toLowerCase()
-      //   );
-      /////
-      //   if (person) {
-      //     if (
-      //       window.confirm(
-      //         `${person.name} is already in the phonebook. Do you want to update the phone number?`
-      //       )
-      //     ) {
-      //       const evtNum = evt.target.elements.number.value;
-      //       const upsertedPerson = {
-      //         ...person,
-      //         name: evtName,
-      //         number: evtNum.length ? evtNum : person.number,
-      //         updated: Date.now(),
-      //       };
-      //       personService
-      //         .update(person.id, upsertedPerson)
-      //         .then((response) => {
-      //           console.log({ updated: response });
-      //           setSuccessMsg(
-      //             `${response.name}'s information was successfully to updated.`
-      //           );
-      //           setTimeout(() => {
-      //             setSuccessMsg(null);
-      //           }, 5000);
-      //           setPersons(
-      //             persons.map((p) =>
-      //               p.name.toLowerCase() !== evtName.toLowerCase() ? p : response
-      //             )
-      //           );
-      //         })
-      //         .catch((error) => {
-      //           setErrorMsg(
-      //             `We were unable to update ${upsertedPerson.name} on the server.`
-      //           );
-      //           console.log({ error });
-      //           setTimeout(() => {
-      //             setErrorMsg(null);
-      //           }, 5000);
-      //           // setPersons(persons.filter(p => p.id !== upsertedPerson.id))
-      //         });
-      //     }
-      //   } else {
-      // const newId = ++persons.map((p) => p.id).sort((a, b) => b - a)[0];
-      const newPerson = {
-        name: evtName,
-        number: evt.target.elements.number.value,
-      };
-      personService
-        .create(newPerson)
-        .then((response) => {
+      const person = persons.find(
+        (p) => p.name.toLowerCase() === evtName.toLowerCase()
+      );
+      ///
+      if (person) {
+        if (
+          window.confirm(
+            `${person.name} is already in the phonebook. Do you want to update the phone number?`
+          )
+        ) {
+          const evtNum = evt.target.elements.number.value;
+          const upsertedPerson = {
+            ...person,
+            name: evtName,
+            number: evtNum.length ? evtNum : person.number,
+          };
+          personService
+            .update(person.id, upsertedPerson)
+            .then((response) => {
+              console.log({ updated: response });
+              setSuccessMsg(
+                `${response.name}'s information was successfully to updated.`
+              );
+              setTimeout(() => {
+                setSuccessMsg(null);
+              }, 5000);
+              setPersons(
+                persons.map((p) =>
+                  p.name.toLowerCase() !== evtName.toLowerCase() ? p : response
+                )
+              );
+            })
+            .catch((error) => {
+              setErrorMsg(
+                `We were unable to update ${upsertedPerson.name} on the server.`
+              );
+              console.log({ error });
+              setTimeout(() => {
+                setErrorMsg(null);
+              }, 5000);
+              // setPersons(persons.filter(p => p.id !== upsertedPerson.id))
+            });
+        }
+      } else {
+        const newId = ++persons.map((p) => p.id).sort((a, b) => b - a)[0];
+        const newPerson = {
+          name: evtName,
+          number: evt.target.elements.number.value,
+        };
+        personService
+          .create(newPerson)
+          .then((response) => {
             evt.target.reset();
-            setTelephoneInputVal('');
-          console.log({ created: response });
-          setSuccessMsg(
-            `${response.name} was successfully to added to the phonebook.`
-          );
-          setTimeout(() => {
-            setSuccessMsg(null);
-          }, 5000);
-          setPersons(persons.concat(response));
-        })
-        .catch((error) => {
-          setErrorMsg(
-            `We were unable to add ${newPerson.name} to the server. ${error.response.data.error}.`
-          );
-          console.log({ error: error.response.data.error });
-          setTimeout(() => {
-            setErrorMsg(null);
-          }, 5000);
-          setPersons(persons.filter((p) => p.id !== newPerson.id));
-        });
-      //   }
+            setTelephoneInputVal("");
+            console.log({ created: response });
+            setSuccessMsg(
+              `${response.name} was successfully to added to the phonebook.`
+            );
+            setTimeout(() => {
+              setSuccessMsg(null);
+            }, 5000);
+            setPersons(persons.concat(response));
+          })
+          .catch((error) => {
+            setErrorMsg(
+              `We were unable to add ${newPerson.name} to the server. ${error.response.data.error}.`
+            );
+            console.log({ error: error.response.data.error });
+            setTimeout(() => {
+              setErrorMsg(null);
+            }, 5000);
+            setPersons(persons.filter((p) => p.id !== newPerson.id));
+          });
+      }
     }
   };
   const handleRemove = (person) => {
     if (window.confirm("Do you really want to remove?")) {
       personService
         .remove(person.id)
-        .then((response) => {
-          console.log({ removed: response });
+        .then((response, b, c) => {
+          console.log({ removed: person, response, b, c });
           setSuccessMsg(`${person.name} was successfully deleted.`);
           setTimeout(() => {
             setSuccessMsg(null);
@@ -178,7 +177,11 @@ const App = () => {
       />
 
       <h3>Add a new</h3>
-      <PersonForm telephoneValue={telephoneInputVal} handleToUpdate={handleInputUpdate} handleToSubmit={handleSubmit} />
+      <PersonForm
+        telephoneValue={telephoneInputVal}
+        handleToUpdate={handleInputUpdate}
+        handleToSubmit={handleSubmit}
+      />
 
       <h3>Numbers</h3>
 
